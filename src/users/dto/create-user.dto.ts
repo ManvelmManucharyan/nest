4fcf8e23cd8b690381b658dba2c387f99ce88321
Manvel Manucharyan <manvelmanucharyan98@gmail.com>
2023-01-29 21:29:22 +0400

    ADD file system

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({ example: "example@email.com", description: "email" })
+    @IsString({ message: "Must be string" })
+    @IsEmail({}, { message: "Not correct email" })
     readonly email: string;
 
     @ApiProperty({ example: "1234", description: "passsword" })
+    @IsString({ message: "Must be string" })
+    @Length(4, 16, { message: "Password must me 4-16 letters" })
     readonly password: string;
 }
